59686384cfccf2866729d63fec99af2df69d8a69
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-02-29 08:15:15 +0800

    Add signup and nav bar for author.

--- app/authors.py
+++ app/authors.py
@@ -1,18 +1,46 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from .models import db, AuthorAccounts, Authors, Articles
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-authors = Blueprint('authors', __name__)
+authors = Blueprint("authors", __name__)
 
 
-@authors.route('/author-options')
+@authors.route("/author-options")
 def options():
-    return render_template('authors/options.html')
+    return render_template("authors/options.html")
 
-@authors.route('/author-login')
+
+@authors.route("/author-login")
 def login():
-    return 'login_sudo'
+    return render_template("authors/authorLogin.html")
 
-    
-@authors.route('/author-signup')
+
+@authors.route("/author-signup")
 def signup():
-    return 'signup'
+    return render_template("authors/authorSignup.html")
+
+
+@authors.route("/author-signup", methods=["POST"])
+def signup_post():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    authorEmail = AuthorAccounts.query.filter_by(email=email).first()
+    authorUsername = AuthorAccounts.query.filter_by(username=username).first()
+
+    if authorUsername or authorEmail:
+        flash("Invalid email or username")
+        return redirect(url_for("authors.signup"))
+
+    authorAccount = AuthorAccounts(
+        username=username,
+        email=email,
+        password=generate_password_hash(password, method="pbkdf2:sha1"),
+    )
+
+    db.session.add(authorAccount)
+    db.session.commit()
+
+    return redirect(url_for("authors.login"))
--- app/models.py
+++ app/models.py
@@ -24,8 +24,9 @@ db = SQLAlchemy(model_class=Base)
 
 class AuthorAccounts(db.Model):
     account_id = Column(Integer, primary_key=True)
-    username = Column(String(100), nullable=False)
+    username = Column(String(100), unique=True, nullable=False)
     password = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
 
 
 class Authors(db.Model):
